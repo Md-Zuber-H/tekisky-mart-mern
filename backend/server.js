b4074d42902c821b0519d9f1116281eef6452117
Md-Zuber-H <zuber13231@gmail.com>
2026-01-03 16:51:31 +0530

    add user model and auth foundation

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./src/config/db.js";
+
 
 dotenv.config();
+connectDB();
+
 
 const app = express();
 
