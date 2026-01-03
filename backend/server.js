689cf8c12569ae8e833d0a931fef7188796ecee7
Md-Zuber-H <zuber13231@gmail.com>
2026-01-03 16:39:06 +0530

    setup express backend server

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("Tekisky Mart Backend is running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
